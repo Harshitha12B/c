839b516de3e78d3eae77c72b6a2c37dc5aa38c34
Harshitha12B <50226968+Harshitha12B@users.noreply.github.com>
2019-05-03 15:02:28 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int get()
+{
+ int a;
+printf("enter the number");
+scanf("%d",&a);
+return a;
+}
+int add(int a)
+{
+int b;
+b=a+1;
+return b;
+}
+void output(int a,int b)
+{
+ printf("the value is %d",b);
+}
+void main()
+{
+int a=get();
+int b=add(a);
+output(a,b);
+}
+
 
